e8a4b497f096c52873e7c8952bd6e0cf75fe8b83
Paul Prozesky <paulp@ska.ac.za>
2015-10-14 13:42:37 +0200

    Hmm. Broken?

--- python/main.py
+++ python/main.py
@@ -8,11 +8,12 @@ LOGGER = logging.getLogger('pysudoku')
 
 blocks = []
 
-def read_puzzles_from_file(filename):
+
+def read_puzzles_from_file(puzfile):
     """
     Read puzzle files as from http://magictour.free.fr/sudoku.htm
     """
-    fptr = open(filename, 'r')
+    fptr = open(puzfile, 'r')
     lines = fptr.readlines()
     fptr.close()
     puzzles = []
@@ -29,82 +30,141 @@ def read_puzzles_from_file(filename):
         puzzles.append(puz)
     return puzzles
 
-# puzzles = read_puzzles_from_file('/home/paulp/projects/sudoku.github/puzzles/subig20.txt')
-puzzles = read_puzzles_from_file('/home/paulp/projects/sudoku.github/puzzles/top870.txt')
-print 'Loaded %i puzzles.' % len(puzzles)
+filename = '/home/paulp/projects/sudoku.github/puzzles/subig20.txt'
+filename = '/home/paulp/projects/sudoku.github/puzzles/top870.txt'
+
+filename = '/home/paulp/projects/code/sudoku.github/puzzles/top870.txt'
+
+puzzles = read_puzzles_from_file(filename)
+print 'Loaded %i puzzles from %s.' % (len(puzzles), filename)
 
 
-def idx_from_addr(addr):
-    return (addr[0] * 9) + addr[1]
+class Puzzle(object):
+    """
+    A Sudoku puzzle.
+    """
 
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.blocks = []
 
-def addr_from_idx(idx):
-    row = idx / 9
-    col = idx - (row * 9)
-    return row, col
+        # make the blocks
+        self.unknown_blocks = []
+        for row in xrange(0, 9):
+            for col in xrange(0, 9):
+                blk = Block(self.blocks, row, col)
+                self.blocks.append(blk)
+                self.unknown_blocks.append(blk.idx)
 
+        # update the blocks from the initial values
+        for idx in range(0, 81):
+            if self.puzzle[idx] > 0:
+                self.blocks[idx].set_value(self.puzzle[idx])
 
-def cel_from_idx(idx):
-    addr = addr_from_idx(idx)
-    return cel_from_addr(addr)
+    def unsolved(self):
+        """
+        How many unsolved blocks are there?
+        :return:
+        """
+        return len(self.unknown_blocks)
 
+    def run_type_1(self):
+        for row in xrange(0, 9):
+            for col in xrange(0, 9):
+                blk = self.blocks[Puzzle.idx_from_addr((row, col))]
+                blk.search_pos_1()
 
-def cel_from_addr(addr):
-    cel_col = addr[1] / 3
-    cel_row = addr[0] / 3
-    return (cel_row * 3) + cel_col
+    def run_pairs(self):
+        for row in xrange(0, 9):
+            for col in xrange(0, 9):
+                blk = self.blocks[Puzzle.idx_from_addr((row, col))]
+                blk.solve_pairs()
 
+    def get_pos(self):
+        # get the possibles again
+        nz_pos = 0
+        for row in xrange(0, 9):
+            rowstring = ''
+            for col in xrange(0, 9):
+                blk = self.blocks[Puzzle.idx_from_addr((row, col))]
+                rowstring += '%3i ' % len(blk.pos)
+                if blk.pos:
+                    nz_pos += 1
+            LOGGER.debug(rowstring)
+        return nz_pos
 
-def celmates(cel=None, idx=None, addr=None):
-    if cel is not None:
-        return _celmates_from_cel(cel)
-    elif idx is not None:
-        return _celmates_from_idx(idx)
-    elif addr is not None:
-        return _celmates_from_addr(addr)
-    raise ValueError('Unspecified action')
+    @staticmethod
+    def idx_from_addr(addr):
+        return (addr[0] * 9) + addr[1]
 
+    @staticmethod
+    def addr_from_idx(idx):
+        row = idx / 9
+        col = idx - (row * 9)
+        return row, col
 
-def _celmates_from_idx(idx):
-    cel = cel_from_idx(idx)
-    return _celmates_from_cel(cel)
+    @staticmethod
+    def cel_from_idx(idx):
+        addr = Puzzle.addr_from_idx(idx)
+        return Puzzle.cel_from_addr(addr)
 
+    @staticmethod
+    def cel_from_addr(addr):
+        cel_col = addr[1] / 3
+        cel_row = addr[0] / 3
+        return (cel_row * 3) + cel_col
 
-def _celmates_from_addr(addr):
-    cel = cel_from_addr(addr)
-    return _celmates_from_cel(cel)
+    @staticmethod
+    def celmates(cel=None, idx=None, addr=None):
+        if cel is not None:
+            return Puzzle._celmates_from_cel(cel)
+        elif idx is not None:
+            return Puzzle._celmates_from_idx(idx)
+        elif addr is not None:
+            return Puzzle._celmates_from_addr(addr)
+        raise ValueError('Unspecified action')
 
+    @staticmethod
+    def _celmates_from_idx(idx):
+        cel = Puzzle.cel_from_idx(idx)
+        return Puzzle._celmates_from_cel(cel)
 
-def _celmates_from_cel(cel_num):
-    col_start = (cel_num % 3) * 3
-    row_start = (cel_num / 3) * 3
-    celmates = []
-    for row in xrange(row_start, row_start + 3):
-        for col in xrange(col_start, col_start + 3):
-            celmates.append(idx_from_addr((row, col)))
-    return celmates
+    @staticmethod
+    def _celmates_from_addr(addr):
+        cel = Puzzle.cel_from_addr(addr)
+        return Puzzle._celmates_from_cel(cel)
 
+    @staticmethod
+    def _celmates_from_cel(cel_num):
+        col_start = (cel_num % 3) * 3
+        row_start = (cel_num / 3) * 3
+        celmates = []
+        for row in xrange(row_start, row_start + 3):
+            for col in xrange(col_start, col_start + 3):
+                celmates.append(Puzzle.idx_from_addr((row, col)))
+        return celmates
 
-def rowmates(row):
-    rowstart = row * 9
-    return range(rowstart, rowstart + 9)
+    @staticmethod
+    def rowmates(row):
+        rowstart = row * 9
+        return range(rowstart, rowstart + 9)
 
-
-def colmates(col):
-    return range(col, 9*9, 9)
+    @staticmethod
+    def colmates(col):
+        return range(col, 9*9, 9)
 
 
 class Block(object):
-    def __init__(self, row, col, val=-1):
-        self.idx = idx_from_addr((row, col))
+    def __init__(self, block_list, row, col, val=-1):
+        self.idx = Puzzle.idx_from_addr((row, col))
         self.row = row
         self.col = col
-        self.cel = cel_from_addr((row, col))
-        self.celmates = celmates(cel=self.cel)
+        self.cel = Puzzle.cel_from_addr((row, col))
+        self.celmates = Puzzle.celmates(cel=self.cel)
         self.celmates.pop(self.celmates.index(self.idx))
-        self.rowmates = rowmates(row=self.row)
+        self.rowmates = Puzzle.rowmates(row=self.row)
         self.rowmates.pop(self.rowmates.index(self.idx))
-        self.colmates = colmates(col=self.col)
+        self.colmates = Puzzle.colmates(col=self.col)
         self.colmates.pop(self.colmates.index(self.idx))
         self.areamates = self.celmates[:]
         self.areamates.extend(self.rowmates)
@@ -114,16 +174,17 @@ class Block(object):
         self.pos = range(1, 10)
         if val > 0:
             self.pos = []
+        self.blks = block_list
 
     def set_value(self, val):
         self.val = val
         self.pos = []
         for idx in self.celmates:
-            blocks[idx].update_pos(val)
+            self.blks[idx].update_pos(val)
         for idx in self.rowmates:
-            blocks[idx].update_pos(val)
+            self.blks[idx].update_pos(val)
         for idx in self.colmates:
-            blocks[idx].update_pos(val)
+            self.blks[idx].update_pos(val)
 
     def update_pos(self, val):
         if len(self.pos) == 0:
@@ -148,13 +209,13 @@ class Block(object):
         def searchmates(mates_idx):
             rowpos = {}
             for idx in mates_idx:
-                for pos in blocks[idx].pos:
+                for pos in self.blks[idx].pos:
                     if pos not in rowpos:
                         rowpos[pos] = []
                     rowpos[pos].append(idx)
             for posval in rowpos:
                 if len(rowpos[posval]) == 1:
-                    blk = blocks[rowpos[posval][0]]
+                    blk = self.blks[rowpos[posval][0]]
                     LOGGER.debug('search_pos_1: block(%i,%i) value -> %i' %
                                  (blk.row, blk.col, posval))
                     blk.set_value(posval)
@@ -164,101 +225,95 @@ class Block(object):
 
     def solve_pairs(self):
         """
-        Look for pairs of possibles
+        Look for pairs of possibles - there can be more than two
+        blocks that have two possibles, but not more than two
+        that have the *same* two possibles!
         :return:
         """
         twopos = []
-        for idx in self.areamates:
-            if len(blocks[idx].pos) == 2:
+        search_area = self.areamates[:]
+        search_area.append(self.idx)
+        for idx in search_area:
+            if len(self.blks[idx].pos) == 2:
                 twopos.append(idx)
+
+        if len(twopos) > 0:
+
+            for idx in range(81):
+                print idx, self.blks[idx].pos, id(self.blks[idx])
+
+            print twopos
+
+            for row in range(9):
+                for col in range(9):
+                    print '%3i' % self.blks[Puzzle.idx_from_addr((row, col))].val,
+                print ''
+
+            raise RuntimeError
+
         if len(twopos) == 2:
-            raise RuntimeError('yay')
+            blk_one = self.blks[twopos[0]]
+            blk_two = self.blks[twopos[1]]
+
+            print blk_one.idx, blk_two.idx, blk_one.pos
+
+            for idx in range(81):
+                    print idx, self.blks[idx].pos
+            raise RuntimeError
+
+            if blk_one.pos == blk_one.pos:
+                search_area_minus = search_area[:]
+
+                print search_area_minus
+
+                search_area_minus.pop(search_area_minus.index(blk_one.idx))
+                search_area_minus.pop(search_area_minus.index(blk_two.idx))
+
+                print search_area_minus
+
+                for idx in search_area_minus:
+                    print idx, self.blks[idx].pos
+
+                    self.blks[idx].update_pos(blk_one.pos[0])
+                    self.blks[idx].update_pos(blk_one.pos[1])
+
+                    print idx, self.blks[idx].pos
+
+                raise RuntimeError
+            else:
+                print blk_one.idx, blk_one.pos
+                print blk_two.idx, blk_two.pos
+                raise RuntimeError('that is odd - should be the same')
+
         elif len(twopos) > 2:
-            raise RuntimeError('this is not possible')
+
+            print twopos
+
+            raise RuntimeError('multiples of two are technically '
+                               'possible... handle this')
 
 
 puz_solved = 0
 puz_failed = 0
-for puzctr in range(0, len(puzzles)):
+# for puzctr in range(len(puzzles)):
+for puzctr in range(314,315):
 
-    puz = puzzles[puzctr]
+    print 'puzzle', puzctr
 
-    # make the blocks
-    blocks = []
-    for row in xrange(0, 9):
-        for col in xrange(0, 9):
-            blk = Block(row, col)
-            blocks.append(blk)
+    puz = Puzzle(puzzles[puzctr])
 
-    # print puz
-    for idx in range(0, 81):
-        if puz[idx] > 0:
-            blocks[idx].set_value(puz[idx])
-
-    # initial values
-    # blocks[idx_from_addr((0, 1))].set_value(5)
-    # blocks[idx_from_addr((0, 3))].set_value(4)
-    # blocks[idx_from_addr((0, 6))].set_value(8)
-    #
-    # blocks[idx_from_addr((1, 2))].set_value(7)
-    # blocks[idx_from_addr((1, 8))].set_value(9)
-    #
-    # blocks[idx_from_addr((2, 0))].set_value(4)
-    # blocks[idx_from_addr((2, 4))].set_value(1)
-    # blocks[idx_from_addr((2, 7))].set_value(6)
-    #
-    # blocks[idx_from_addr((3, 3))].set_value(7)
-    # blocks[idx_from_addr((3, 5))].set_value(9)
-    # blocks[idx_from_addr((3, 8))].set_value(8)
-    #
-    # blocks[idx_from_addr((4, 2))].set_value(6)
-    # blocks[idx_from_addr((4, 4))].set_value(4)
-    # blocks[idx_from_addr((4, 6))].set_value(7)
-    #
-    # blocks[idx_from_addr((5, 0))].set_value(5)
-    # blocks[idx_from_addr((5, 3))].set_value(1)
-    # blocks[idx_from_addr((5, 5))].set_value(2)
-    #
-    # blocks[idx_from_addr((6, 1))].set_value(6)
-    # blocks[idx_from_addr((6, 4))].set_value(2)
-    # blocks[idx_from_addr((6, 8))].set_value(4)
-    #
-    # blocks[idx_from_addr((7, 0))].set_value(3)
-    # blocks[idx_from_addr((7, 6))].set_value(2)
-    #
-    # blocks[idx_from_addr((8, 2))].set_value(8)
-    # blocks[idx_from_addr((8, 5))].set_value(3)
-    # blocks[idx_from_addr((8, 7))].set_value(5)
+    for row in range(9):
+        for col in range(9):
+            print '%3i' % puz.blocks[Puzzle.idx_from_addr((row, col))].val,
+        print ''
 
     for loopctr in range(0, 1):
 
-        # # show the possibles
-        # for row in xrange(0, 9):
-        #     for col in xrange(0, 9):
-        #         blk = blocks[idx_from_addr((row, col))]
-        #         print '%3i ' % len(blk.pos),
-        #     print ''
+        # run searches
+        puz.run_type_1()
+        # puz.run_pairs()
 
-        # run a type 1 search
-        for row in xrange(0, 9):
-            for col in xrange(0, 9):
-                blk = blocks[idx_from_addr((row, col))]
-                blk.search_pos_1()
-
-        for row in xrange(0, 9):
-            for col in xrange(0, 9):
-                blk = blocks[idx_from_addr((row, col))]
-                # blk.solve_pairs()
-
-        # show the possibles again
-        nonzero_pos = 0
-        for row in xrange(0, 9):
-            for col in xrange(0, 9):
-                blk = blocks[idx_from_addr((row, col))]
-                # print '%3i ' % len(blk.pos),
-                if blk.pos:
-                    nonzero_pos += 1
-            # print ''
+        nonzero_pos = puz.get_pos()
         if nonzero_pos > 0:
             # print 'puzzle %06i: NOT SOLVED' % puzctr
             puz_failed += 1
@@ -275,9 +330,13 @@ for puzctr in range(0, len(puzzles)):
 
         # print 80 * '*'
 
-    continue
+print ''
+for row in range(9):
+    for col in range(9):
+        print '%3i' % puz.blocks[Puzzle.idx_from_addr((row, col))].val,
+    print ''
 
-print puz_solved, puz_failed
+print 'solved(%i) failed(%i)' % (puz_solved, puz_failed)
 
     # # print possibles
     # #
@@ -335,3 +394,39 @@ print puz_solved, puz_failed
     # for row in xrange(0, 9):
     #     for col in xrange(0, 9):
     #         print row, col, cell_from_index(row, col), possibles[row][col]
+
+
+    # initial values
+    # blocks[idx_from_addr((0, 1))].set_value(5)
+    # blocks[idx_from_addr((0, 3))].set_value(4)
+    # blocks[idx_from_addr((0, 6))].set_value(8)
+    #
+    # blocks[idx_from_addr((1, 2))].set_value(7)
+    # blocks[idx_from_addr((1, 8))].set_value(9)
+    #
+    # blocks[idx_from_addr((2, 0))].set_value(4)
+    # blocks[idx_from_addr((2, 4))].set_value(1)
+    # blocks[idx_from_addr((2, 7))].set_value(6)
+    #
+    # blocks[idx_from_addr((3, 3))].set_value(7)
+    # blocks[idx_from_addr((3, 5))].set_value(9)
+    # blocks[idx_from_addr((3, 8))].set_value(8)
+    #
+    # blocks[idx_from_addr((4, 2))].set_value(6)
+    # blocks[idx_from_addr((4, 4))].set_value(4)
+    # blocks[idx_from_addr((4, 6))].set_value(7)
+    #
+    # blocks[idx_from_addr((5, 0))].set_value(5)
+    # blocks[idx_from_addr((5, 3))].set_value(1)
+    # blocks[idx_from_addr((5, 5))].set_value(2)
+    #
+    # blocks[idx_from_addr((6, 1))].set_value(6)
+    # blocks[idx_from_addr((6, 4))].set_value(2)
+    # blocks[idx_from_addr((6, 8))].set_value(4)
+    #
+    # blocks[idx_from_addr((7, 0))].set_value(3)
+    # blocks[idx_from_addr((7, 6))].set_value(2)
+    #
+    # blocks[idx_from_addr((8, 2))].set_value(8)
+    # blocks[idx_from_addr((8, 5))].set_value(3)
+    # blocks[idx_from_addr((8, 7))].set_value(5)
